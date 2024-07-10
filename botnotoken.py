38d695d3a2860ae793ccd507759649a95c4c4e62
jblades2 <44749034+jblades2@users.noreply.github.com>
2024-07-09 22:44:18 -0400

    Add files via upload

---
+++ botnotoken.py
@@ -0,0 +1,93 @@
+import discord
+from discord.ext import commands
+import json   # json
+import re     # regex
+import time   # sleep
+import typing
+from urllib.request import Request, urlopen # HTML request
+from http.cookiejar import CookieJar
+
+#####
+# Setup URL opener
+import urllib.request
+
+class SafeOpener(urllib.request.OpenerDirector):
+  def __init__(self, handlers: typing.Iterable = None):
+    super().__init__()
+    handlers = handlers or (
+      urllib.request.UnknownHandler,
+      urllib.request.HTTPDefaultErrorHandler,
+      urllib.request.HTTPRedirectHandler,
+      urllib.request.HTTPSHandler,
+      urllib.request.HTTPErrorProcessor,
+      urllib.request.HTTPHandler,
+      urllib.request.HTTPCookieProcessor,
+    )
+
+    for handler_class in handlers:
+      self.add_handler(handler_class())
+
+# KoV's guild swgoh ID
+guildId = "nzYxnRYNRYCT8wRhEJcWpw"
+
+request_urls = []
+opener = SafeOpener()
+urllib.request.install_opener(opener)
+
+#403 fix
+#https://stackoverflow.com/questions/13303449/urllib2-httperror-http-error-403-forbidden
+httpOptions = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+      'Accept-Encoding': 'none',
+      'Accept-Language': 'en-US,en;q=0.8',
+#      'Connection': 'keep-alive',
+      'method': 'get',
+      'muteHttpExceptions': False,
+  }
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+    await bot.tree.sync()
+
+@bot.tree.command(name="hello",description="Surely")
+async def slash_command(interaction:discord.Interaction):
+    await interaction.response.send_message("Hello There!")
+
+@bot.tree.command(name="guildid",description="Gets basic guild info")
+async def guild_slash_command(interaction:discord.Interaction):
+        # Request general guild info
+    site = "https://swgoh.gg/api/guild-profile/" + str(guildId)
+    req = Request(url=site,headers=httpOptions)
+    text = opener.open(req).read()
+    basicGuildInfo = json.loads(text)
+      #Extract Guild Name and Print
+    await interaction.response.send_message(f"Guild name is " + basicGuildInfo["data"]["name"])
+
+#Calculate percent armor boost from DCs and Abilites, print result as final Armor percentage
+@bot.tree.command(name="armor_boost", description="Calculate Final Armor Value after DCs and Abilities")
+async def defense_boost(interaction: discord.Interaction, base_armor: int, character_level: int, percent_defense_boost: int, percent_defense_boost2: typing.Optional[int] = 0, percent_defense_boost3: typing.Optional[int] = 0, percent_defense_boost4: typing.Optional[int] = 0):
+    initial_defense = (base_armor * (character_level * 7.5)) / (100 - base_armor)
+    new_defense = initial_defense + (initial_defense * (percent_defense_boost / 100)) + (initial_defense * (percent_defense_boost2 / 100)) + (initial_defense * (percent_defense_boost3 / 100)) + (initial_defense * (percent_defense_boost4 / 100))
+    final_armor = (new_defense * 100) / (new_defense + (character_level * 7.5))
+    await interaction.response.send_message(f"Final Armor Stat: {final_armor}" + "%")
+
+@bot.tree.command(name="za_boost", description="Calculate final offense after ZA boosts")
+async def za_command(interaction: discord.Interaction, base_offense: int, base_health: int, percent_za_boost: int, percent_za_boost2: typing.Optional[int] = 0, percent_za_boost3: typing.Optional[int] = 0, percent_za_boost4: typing.Optional[int] = 0, percent_za_boost5: typing.Optional[int] = 0):
+    za_offense_boost = (base_offense + (base_health * (percent_za_boost / 100)) + (base_health * (percent_za_boost2 / 100)) + (base_health * (percent_za_boost3 / 100)) + (base_health * (percent_za_boost4 / 100)) + (base_health * (percent_za_boost5 / 100)))
+    new_offense = base_offense + za_offense_boost
+    await interaction.response.send_message(f"Final Offense after ZA Applied = {new_offense}")
+
+
+
+
